12f547baf5885cd0cfb6ba68af01043b6626f19f
Graham Ollis <plicease@cpan.org>
2020-09-30 06:35:21 -0600

    version 6.06_01

--- lib/File/Listing.pm
+++ lib/File/Listing.pm
@@ -6,7 +6,8 @@ use Carp ();
 use HTTP::Date qw(str2time);
 use base qw( Exporter );
 
-our $VERSION = "6.04";
+our $VERSION = '6.06_01';
+$VERSION =~ tr/_//d;
 sub Version { $VERSION; }
 
 our @EXPORT = qw(parse_dir);
